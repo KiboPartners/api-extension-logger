73d8621156304021a9ff340d46e83d777dcb08d4
TylerBrinkman <tyler.brinkman@kibocommerce.com>
2024-10-12 11:18:17 -0500

    basic logging

--- v1/app.js
+++ v1/app.js
@@ -1,5 +1,16 @@
 const http = require('http');
 const port = 8000;
 const host = 'localhost';
-const server = http.createServer(function (req, res) {res.end()});
+const server = http.createServer(function (req, res) {
+  let body = ''
+
+  req.on('data', chunk =>{
+    body += chunk.toString()
+  })
+
+  req.on('end', () => {
+    console.log(JSON.parse(body))
+  })
+  res.end()
+});
 server.listen(port, host, function () {});
